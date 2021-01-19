2b9cbb6326909bddcda64ecf90a4f30e7d270d1e
ASokry <anthonysokry11@gmail.com>
2021-01-19 12:38:07 -0800

    React app and packages

---
+++ src/Home.js
@@ -0,0 +1,22 @@
+import './css/Navbar.css';
+import './css/Home.css';
+
+function Home() {
+  return (
+    <div className="home">
+      <div className="navbar">
+        <h1>This is a navbar</h1>
+      </div>
+
+      <div className="home-intro">
+        <h1>This is the Home page intro portion (hero images, intro paragraph, etc.)</h1>
+      </div>
+
+      <div className="home-body">
+        <h2>Body of home page</h2>
+      </div>
+    </div>
+  );
+}
+
+export default Home;
