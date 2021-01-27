b7d5a5bfc4a636d3b9fadc405f3be42baf2b370a
ASokry <anthonysokry11@gmail.com>
2021-01-26 17:27:43 -0800

    Link tag in home page

--- src/Home.js
+++ src/Home.js
@@ -8,10 +8,16 @@ function Home() {
     <div className="home-page">
       <div className="navbar">
         <h1>This is a navbar</h1>
+        <Link to="/about">A</Link>
       </div>
 
       <div className="home-intro">
         <h1>This is the Home page intro portion (hero images, intro paragraph, etc.)</h1>
+      </div>
+
+      <div>
+        <img></img>
+        <p></p>
       </div>
 
       <div className="home-body">
