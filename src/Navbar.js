68707003837e7be85d1b6155d28b27915b7be453
ASokry <anthonysokry11@gmail.com>
2021-02-03 19:53:50 -0800

    nav mobile hide and show

--- src/Navbar.js
+++ src/Navbar.js
@@ -1,58 +1,77 @@
 
 import './css/Navbar.css';
-import React, {Component, useState, useEffect} from 'react';
+import React, {useState, useEffect} from 'react';
 import { Link } from 'react-router-dom';
 
-function webMode() {
-    // const [winWidth, setWinWidth] = useState(window.innerWidth);
-    const winWidth = window.innerWidth;
-    // useEffect(() => {
-    //     const handleResize = () => {
-    //         setWinWidth(window.innerWidth);
-    //     };
-    //     window.addEventListener("resize", handleResize);
-    // }, 0);
-    
-    if(winWidth > 600){
-        return "navbar-collapse";
-    }
-    else {
-        return "navbar-collapse-mobile";
-    }
-}
+function Navbar() {
+    const [winWidth, setWinWidth] = useState(window.innerWidth);
+    useEffect(() => {
+        const handleResize = () => {
+            setWinWidth(window.innerWidth);
+        };
+        window.addEventListener("resize", handleResize);
+    });
+    const [cllickCollapse, setClickCollapse] = useState(false);
 
-class Navbar extends Component {
+    function webMode() {
+        if(winWidth > 600){
+            return "navbar-collapse";
+        }
+        else {
+            return "navbar-collapse-mobile";
+        }
+    }
 
-    render() {
-        return(
-            <div className="navbar">
-                <div className="navbar-header">
+    const [eleHeight, setEleHeight] = useState(0);
+    const navHidden = {
+        marginTop: eleHeight
+    };
+
+    const navShow = {
+        marginTop: "52px"
+    };
+
+    function jsStyle(){
+        if(winWidth <= 600 && cllickCollapse == true){
+            return navShow;
+        }
+        return navHidden;
+    }
+
+    return(
+        <div className="navbar">
+            <div className="navbar-header">
                 <Link className="navbar-title" to="/">
                     <img className="navbar-logo" src="https://asokry.github.io/cadre-mentorship-snapshot2020/img/logo_draft.png"/>
                 </Link>
-                <button type="button" className="navbar-toggle" data-toggle="collapse" data-target="#myNavbar">
+                <button type="button" className="navbar-toggle" onClick={()=>{setClickCollapse(!cllickCollapse)}}>
                     <span className="icon-bar"></span>
                     <span className="icon-bar"></span>
                     <span className="icon-bar"></span>                        
                 </button>
-                </div>
-                <div className={webMode()}>
-                    <ul className="navbar-center">
-                    <li><Link to="/">Home</Link></li>
-                        <li><Link to="/">About</Link></li>
-                        <li><Link to="/">Forms</Link></li>
-                        <li><Link to="/">FAQ</Link></li>
-                        <li><Link to="/">Forms</Link></li>
-                        <li><Link to="/">FAQ</Link></li>
-                    </ul>
-                    <ul className="navbar-right">
-                        <li><Link to="/">F20</Link></li>
-                        <li><Link to="/">SP21</Link></li>
-                    </ul>
-                </div>
             </div>
-        )
-    }
+            <div className={webMode()} style={jsStyle()}
+                ref={el => {
+                    if (!el) return;
+                    console.log(-el.getBoundingClientRect().height);    
+                    setEleHeight(-(el.getBoundingClientRect().height + 1));
+                }}
+            >
+                <ul className="navbar-center">
+                <li><Link to="/">Home</Link></li>
+                    <li><Link to="/">About</Link></li>
+                    <li><Link to="/">Forms</Link></li>
+                    <li><Link to="/">FAQ</Link></li>
+                    {/* <li><Link to="/">Forms</Link></li>
+                    <li><Link to="/">FAQ</Link></li> */}
+                </ul>
+                <ul className="navbar-right">
+                    <li><Link to="/">F20</Link></li>
+                    <li><Link to="/">SP21</Link></li>
+                </ul>
+            </div>
+        </div>
+    )
 }
 
 export default Navbar;
