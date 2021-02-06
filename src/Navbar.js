98f13887a149747486a6aba3c6fe61e76a3fb396
ASokry <anthonysokry11@gmail.com>
2021-02-06 12:51:28 -0800

    navbar polish

--- src/Navbar.js
+++ src/Navbar.js
@@ -4,36 +4,7 @@ import React, {useState, useEffect} from 'react';
 import { Link } from 'react-router-dom';
 
 function Navbar() {
-    const [winWidth, setWinWidth] = useState(window.innerWidth);
-    useEffect(() => {
-        const handleResize = () => {
-            setWinWidth(window.innerWidth);
-        };
-        window.addEventListener("resize", handleResize);
-    });
     const [cllickCollapse, setClickCollapse] = useState(false);
-
-    // function webMode() {
-    //     if(winWidth > 600){
-    //         return "navbar-collapse";
-    //     }
-    //     else {
-    //         return "navbar-collapse-mobile";
-    //     }
-    // }
-
-    const [eleHeight, setEleHeight] = useState(0);
-    const navHidden = {height: 0};
-    const navShow = {height: eleHeight};
-
-    function jsStyle(){
-        if(winWidth <= 600 && cllickCollapse == true){
-            return navShow;
-        }
-        else if (winWidth <= 600 && cllickCollapse == false){
-            return navHidden;
-        }
-    }
 
     return(
         <div className="navbar">
@@ -48,15 +19,7 @@ function Navbar() {
                         <span className="icon-bar"></span>                        
                     </button>
                 </div>
-                {/* style={jsStyle()}  */}
-                {/* winWidth > 600 ? "navbar-collapse" : "navbar-collapse-mobile" */}
-                <div className={cllickCollapse ? "navbar-collapse active" : "navbar-collapse"}
-                    ref={el => {
-                        if (!el) return;
-                        console.log(-el.getBoundingClientRect().height);    
-                        setEleHeight(-(el.getBoundingClientRect().height + 1));
-                    }}
-                >
+                <div className={cllickCollapse ? "navbar-collapse active" : "navbar-collapse"}>
                     <ul className="navbar-center">
                     <li><Link to="/">Home</Link></li>
                         <li><Link to="/">About</Link></li>
