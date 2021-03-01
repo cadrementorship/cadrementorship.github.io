47aca5cc4fd1cc5419d70f175d48c5a039878304
ASokry <anthonysokry11@gmail.com>
2021-03-01 15:39:54 -0800

    faq started

--- src/FAQ.js
+++ src/FAQ.js
@@ -8,11 +8,50 @@ function FAQ() {
       <Navbar/>
 
       <div className="faq-intro">
-        <h1>This is the faq page intro portion</h1>
+        <div className="faq-hero">
+          <img className="faq-hero-img" src="https://dummyimage.com/368x200.jpg"/>
+          <p className="faq-hero-text">Anthony Sokry was Here, Lorem Lorem Lorem</p>
+        </div>
+        <hr className="faq-intro-line"/>
+        <p className="faq-intro-text">The Cadre Mentorship Program is an academic leadership program that helps Digital Media Arts students thrive in thier careers at San Jose State University and beyond.</p>
       </div>
 
       <div className="faq-body">
-        <h2>Body of faq page</h2>
+        <div className="faq-body-all-questions">
+          <h1 className="faq-body-questions-title">Frequently Asked Questions</h1>
+          <ul className="faq-body-list">
+            <li><p className="faq-body-question">Question 1</p></li>
+            <li><p className="faq-body-question">Question 1</p></li>
+            <li><p className="faq-body-question">Question 1</p></li>
+            <li><p className="faq-body-question">Question 1</p></li>
+          </ul>
+        </div>
+
+        <div className="faq-body-all-answers">
+          <div className="faq-body-answer">
+            <h2 className="faq-body-answer-title">Question here</h2>
+            <p className="faq-body-answer-text">Answer here</p>
+            <p className="faq-body-answer-back">Link to go back to questions list</p>
+          </div>
+
+          <div className="faq-body-answer">
+            <h2 className="faq-body-answer-title">Question here</h2>
+            <p className="faq-body-answer-text">Answer here</p>
+            <p className="faq-body-answer-back">Link to go back to questions list</p>
+          </div>
+
+          <div className="faq-body-answer">
+            <h2 className="faq-body-answer-title">Question here</h2>
+            <p className="faq-body-answer-text">Answer here</p>
+            <p className="faq-body-answer-back">Link to go back to questions list</p>
+          </div>
+
+          <div className="faq-body-answer">
+            <h2 className="faq-body-answer-title">Question here</h2>
+            <p className="faq-body-answer-text">Answer here</p>
+            <p className="faq-body-answer-back">Link to go back to questions list</p>
+          </div>
+        </div>
       </div>
 
       <div className="footer">
